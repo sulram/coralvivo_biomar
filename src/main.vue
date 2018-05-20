02df58764a5f45c6d286aaf5bca50a32c35f4be8
marlus <mar251@gmail.com>
2018-05-19 21:33:47 -0300

    edits

---
+++ src/main.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>Hello from {{text}}</h2>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+       text: '',
+       state: 'menu'
+    }
+  },
+  mounted: function(){
+  	console.log("app mounted")
+  	var uluru = {lat: -14.000, lng: -37.000};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: uluru,
+      mapTypeId: 'satellite',
+      disableDefaultUI: true,
+      zoomControl: true,
+      zoomControlOptions: {
+        position: google.maps.ControlPosition.LEFT_BOTTOM
+      },
+      scaleControl: true
+    });
+  }
+}
+</script>
