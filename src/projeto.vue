ce83abc31fac6c1d8f1385b7e703603fb78667fd
marlus <mar251@gmail.com>
2018-05-29 21:43:11 -0300

    single projeto

--- src/projeto.vue
+++ src/projeto.vue
@@ -1,15 +1,79 @@
 <template>
+
   <div class="single-projeto" v-if="content">
     
-    <div class="title">
-      {{content.nome}}
+    <div class="single-projeto-header columns is-vcentered has-text-centered">
+      <div class="column">
+        <div class="title">
+          {{content.nome}}
+        </div>
+        <div class="desde fancy">
+          <span>desde {{ content.desde }}</span>
+        </div>
+      </div>
+      <div class="column is-narrow">
+        <div class="marca">
+          <img v-bind:src="logo" height="120">
+        </div>
+      </div>
     </div>
 
-    <button v-on:click="$emit('backToMenu')">Voltar</button>
+    <div class="single-projeto-marcas columns">
+      <div class="tab column is-narrow" v-if="content.proponentes">
+        <p v-if="content.proponentes.length == 1">Proponente</p>
+        <p v-if="content.proponentes.length > 1">Proponentes</p>
+        <img v-for="(item, index) in content.proponentes" v-bind:src="logoPath + content.proponentes[index]">
+      </div>
+      <div class="tab column is-narrow" v-if="content.patrocinio">
+        <p>Patrocínio</p>
+        <img v-for="(item, index) in content.patrocinio" v-bind:src="logoPath + content.patrocinio[index]">
+      </div>
+    </div>
 
-    <img v-bind:src="thumb">
+    <div class="single-projeto-content columns">
+
+      <div class="content-main column is-8">
+        
+        <transition name="fade">
+          <div class="content-main-page" v-if="state === 'sobre'">
+            {{ content.sobre }}
+          </div>
+        </transition>
+
+        <transition name="fade">
+          <div class="content-main-page" v-if="state === 'resultados'">
+            {{ content.resultados }}
+          </div>
+        </transition>
+
+      </div>
+
+      <div class="content-nav column is-4 has-text-centered">
+        
+        <ul>
+          <li><a v-on:click="selectTab('sobre')">Sobre</a></li>
+          <li><a v-on:click="selectTab('resultados')">Resultados</a></li>
+          <li><a v-on:click="selectTab('colaboradores')">Colaboradores</a></li>
+          <li><a v-on:click="selectTab('contato')">Contato</a></li>
+        </ul>
+
+        <button v-on:click="$emit('backToMenu')">Voltar</button>
+
+      </div>
+
+    </div>
+
+    <div class="single-projeto-vis-selector">
+      <div class="columns">
+        <div class="column"></div>
+        <div class="column">
+          <img v-bind:src="thumb">
+        </div>
+      </div>
+    </div>    
     
   </div>
+
 </template>
 
 
@@ -22,7 +86,8 @@
     
     data () {
         return {
-          content: null
+          content: null,
+          state: 'sobre'
         }
       },
 
@@ -35,12 +100,22 @@
       },
 
       methods: {
-
+        selectTab(val) {
+          this.state = val
+        }
       },
 
       computed: {
-        thumb () {
+        thumb() {
           return store.get('pGalleryPath') + this.content.fotos[0]
+        },
+
+        logoPath() {
+          return store.get('uLogoPath')
+        },
+
+        logo() {
+          return store.get('uLogoPath') + this.content.logo
         }
       }
   }
